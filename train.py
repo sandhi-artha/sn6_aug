9102487f0c464988c936283e2d211c0a2fff63be
sandhi_artha <sandiartha99@yahoo.co.id>
2021-12-21 00:18:31 +0100

    init tpu in notebook

--- train.py
+++ train.py
@@ -39,16 +39,7 @@ else:
 
 
 
-### INIT TPU ###
-try:
-    tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
-    print('Device:', tpu.master())
-    strategy = tf.distribute.experimental.TPUStrategy(tpu)  # set distribution strategy
-except:
-    tpu = None
-    print('not using TPU')
-    strategy = tf.distribute.get_strategy()
-print("REPLICAS: ", strategy.num_replicas_in_sync)
+
 
 
 ### GLOBAL VARIABLES ###
