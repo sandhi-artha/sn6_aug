f6c9cf41da74229ab211e7202cde6c1598221eea
sandhi_artha <sandiartha99@yahoo.co.id>
2021-12-20 22:06:43 +0100

    dataloader

--- cfg.py
+++ cfg.py
@@ -10,7 +10,30 @@ dg_cfg = {
 }
 
 tr_cfg = {
-
+    # log cfg
+    'run'        : 4,   # REMEMBER TO INCREMENT!!
+    'COMMENTS'   : 'external validation',
+    
+    # dataset cfg
+    'IMAGE_DIM'  : 900,  # must be the same as tfrecord res
+    'IMAGE_RS'   : 640,
+    'TRAIN_PATH' : 'sn6-900-uint8',  # kaggle ds name
+    'VAL_PATH'   : 'base-val-8',
+    'IS_FP32'    : 0,       # 1 if train or validating on fp32
+    'SAR_CH'     : [1,4,3], # [0,3,2],      # HH, VV, VH. 0 = all channel
+    
+    # training cfg
+    'DEVICE'     : 'tpu',
+    'SEED'       : 17,
+    'BACKBONE'   : 'effb4',            # 'effb4', 'res50'
+    'ARC'        : 'fpn',              # 'unet', 'fpn'
+    'WEIGHT'     : 'imagenet',         # 'imagenet', 'pre-trained from:..', None
+    'LF'         : 'bce',    # 'bce', 'jaccard_distance', 'focal', 'giou'
+    'EPOCHS'     : 60,
+    'L_RATE'     : 5e-5,       # 5e-5
+    'IS_3_FOLD'  : 0,   # do same training 3x to get an average value
+    'IS_CB_ES'   : 0,   # early stopping
+    'IS_CB_LRS'  : 0,   # learning rate scheduler, if false uses lr_ramp
 }
 
 
--- viz.py
+++ viz.py
@@ -1,1 +1,11 @@
 # viz batched dataset, plot history, plot prediction results
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_example(img, mask):
+    f,ax = plt.subplots(1,2,figsize=(10,5))
+    ax[0].imshow(img)
+    if len(mask.shape)==3:
+        mask = np.squeeze(mask, axis=-1)
+    ax[1].imshow(mask, cmap='gray')
+    plt.show()
