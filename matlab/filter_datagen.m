4e14089233c1d2c52fda5b3b0c01130a41cc8c5e
sandhi_artha <sandiartha99@yahoo.co.id>
2022-01-28 16:27:06 +0100

    matlab filter from crop

--- matlab/filter_datagen.m
+++ matlab/filter_datagen.m
@@ -1,48 +1,60 @@
 % read rasters from orientation 1
-DATA_DIR = 'D:\Projects\python\dataset\spacenet6-challenge\AOI_11_Rotterdam\SAR-Intensity';
-BASE_SAVE_DIR = 'D:\Projects\python\dataset\sn6_aug\filter';
-filenames1 = readlines("orient1_fp.txt", "EmptyLineRule","skip");
+load('fps1_5folds.mat')
 
+DATA_DIR = 'D:\Projects\python\dataset\sn6_aug\hh_crop';
+BASE_SAVE_DIR = 'D:\Projects\python\dataset\sn6_aug\filter_crop';
+% filenames1 = readlines("orient1_fp.txt", "EmptyLineRule","skip");
+
+folds = ["fold0", "fold1", "fold4"];
+folds_cell = {fold0; fold1; fold4};
 
 filt_list = ["elee" "frost" "gmap"];
 win_list = [3 5 7];
 
-for i = 1:length(filenames1)
-    fprintf('tile %d from %d\n', i, length(filenames1))
-    fp = append(DATA_DIR, '\', filenames1(i));
+for f = 1:length(folds)
+    fold = folds_cell{f};  % to access cell's content use {}
+    fold_name = folds(f);
+    fprintf('fold: %s\n', fold_name)
 
-    % read raster
-    [sar,r] = readgeoraster(fp);
-    % read just HH and convert to linear scale
-    sar_hh = to_lin(sar(:,:,1));
+    for i = 1:length(fold)
+        fprintf('tile %d from %d\n', i, length(fold))
+        % to equal space, some filenames have trailing space -> strtrim
+        fp = append(DATA_DIR, '\', fold_name, '\', strtrim(fold(i,:)));
+        % read cropped sar_hh tif and convert to linear
+        sar_hh = to_lin(imread(fp));
 
-    % loop through all selected filters
-    for j = 1:length(filt_list)
-        %loop through all window_size
-        for k = 1:length(win_list)
-            filt = filt_list(j);
-            win = win_list(k);
-            sar_res = sarimg_despeckling_filter(filt, sar_hh, 1, win, 2);
-            
-            % silence p-regen warning, only need to do once per session
-            warning('off','last');
-            
-            % handle when output is below 1 (nodata value in linear)
-            % prevents inf when converting back to dB if there's value 0
-            sar_res(sar_res < 1) = 1;
-            
-            % convert to dB again and rescale to range of uint8
-            sar_res = uint8(rescale(to_db(sar_res), 0, 255));
+        % loop through all selected filters
+        for j = 1:length(filt_list)
+            %loop through all window_size
+            for k = 1:length(win_list)
+                filt = filt_list(j);
+                win = win_list(k);
+                sar_res = sarimg_despeckling_filter(filt, sar_hh, 1, win, 2);
 
-            % save to filter folder with same filename as original
-            save_fn = strrep(filenames1(i), '.tif','.png');
-            save_dir = append(BASE_SAVE_DIR, '\', filt, '\', num2str(win));
-            status = mkdir(save_dir);
-            save_fp = append(save_dir, '\', save_fn);
-            imwrite(sar_res, save_fp)
+                % silence p-regen warning, only need to do once per session
+                warning('off','last');
+
+                % handle when output is below 1 (nodata value in linear)
+                % prevents inf when converting back to dB if there's value 0
+                sar_res(sar_res < 1) = 1;
+
+                % convert to dB again and rescale to range of uint8
+                sar_res = single(to_db(sar_res));
+%                 sar_res = uint8(rescale(to_db(sar_res), 0, 255));
+
+                % save to filter folder with same filename as original
+                save_fn = strrep(strtrim(fold(i,:)), '.tif','.mat');
+                save_dir = append(BASE_SAVE_DIR, '\', filt, '\', num2str(win));
+                status = mkdir(save_dir);
+                save_fp = append(save_dir, '\', save_fn);
+                save(save_fp, 'sar_res')
+            end
         end
     end
 end
+
+
+
 
 
 function y_db = to_db(y)
