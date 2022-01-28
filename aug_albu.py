d60827383c91f9d4b40af77f93cd32e538955fbc
sandhi_artha <sandiartha99@yahoo.co.id>
2022-01-28 16:28:15 +0100

    organized aug_tf and aug_albu

---
+++ aug_albu.py
@@ -0,0 +1,81 @@
+import albumentations as A
+import albumentations.augmentations.transforms as AAT
+
+### GLOBAL VARIABLES ###
+IS_AUG_ALBU = 0
+
+
+
+### AUGMENTATION TRANSFORMS ###
+def get_aug_albu_func(
+    is_mot_blur=0,
+    is_sharpen=0,
+    is_clahe=0,
+    is_gaus_noise=0,
+    is_speckle_noise=0,
+    is_coarse_do=0,
+):
+    """transformation list for augmentations in albu"""
+    # update cfg
+    tr_cfg['IS_MOT_BLUR'] = is_mot_blur
+    tr_cfg['IS_SHARPEN'] = is_sharpen
+    tr_cfg['IS_CLAHE'] = is_clahe
+    tr_cfg['IS_GAUS_NOISE'] = is_gaus_noise
+    tr_cfg['IS_SPECKLE_NOISE'] = is_speckle_noise
+    tr_cfg['IS_COARSE_DO'] = is_coarse_do
+
+
+    one_of_transforms = []
+    transforms_list = []
+
+    # pixel transformation
+    if is_mot_blur:
+        one_of_transforms.append(AAT.MotionBlur(blur_limit=12))
+    if is_sharpen:
+        one_of_transforms.append(
+            AAT.Sharpen(alpha=(0.1,0.4), lightness=(.9,1.0))
+        )
+    if is_clahe:
+        one_of_transforms.append(
+            A.Sequential([
+            AAT.FromFloat(dtype='uint8', max_value=255),
+            AAT.CLAHE(clip_limit=4.0),
+            AAT.ToFloat(max_value=255)])
+        )
+    if is_gaus_noise:
+        one_of_transforms.append(AAT.GaussNoise(var_limit=.01))
+    if is_speckle_noise:
+        one_of_transforms.append(
+            AAT.MultiplicativeNoise(multiplier=(0.8,1.2), elementwise=True)
+        )
+    
+
+    if len(one_of_transforms) > 0:
+        transforms_list.append(A.OneOf(one_of_transforms))
+    
+    # geometric transformation, done after filtering
+    if is_coarse_do:
+        transforms_list.append(
+            A.augmentations.CoarseDropout(max_holes=10, max_height=20,
+                max_width=10, min_holes=1, min_height=5, min_width=5,
+                fill_value=0.9, mask_fill_value=0)
+        )
+    
+    if len(transforms_list)>0:
+        IS_AUG_ALBU = 1
+    else:
+        IS_AUG_ALBU = 0
+
+    return A.Compose(transforms_list)
+
+def aug_transform(image, label):
+    """spatial transformation done on both image and label
+    pixel trans only on image"""
+    transform_data = TRANSFORMS(image=image, mask=label)
+    return transform_data['image'], transform_data['mask']
+
+def aug_albu(image, label):
+    """wrapper to feed numpy format data to TRANSFORMS"""
+    aug_img, aug_mask = tf.numpy_function(
+        func=aug_transform, inp=[image, label], Tout=[tf.float32, tf.float32])
+    return aug_img, aug_mask
---
+++ aug_tf.py
@@ -0,0 +1,207 @@
+import tensorflow as tf
+import math
+
+### GLOBAL VARIABLES ###
+IS_AUG_TF = 0
+IS_OFF_AUG = 0
+
+### REDUCE RESIZE FUNCTION ###
+def resize(image, label):
+    """center crop and resize to size IMAGE_RS"""
+    image = tf.image.resize(image, [tr_cfg['IMAGE_RS'], tr_cfg['IMAGE_RS']])
+    label = tf.image.resize(label, [tr_cfg['IMAGE_RS'], tr_cfg['IMAGE_RS']])
+    return image, label
+
+def pad_resize(image, label):
+    """add paddings to create the original square image but resized"""
+    image = tf.image.resize_with_pad(image, tr_cfg['IMAGE_RS'], tr_cfg['IMAGE_RS'])
+    label = tf.image.resize_with_pad(label, tr_cfg['IMAGE_RS'], tr_cfg['IMAGE_RS'])
+    return image, label
+
+def random_crop(image, label):
+    """crops a patch of size IMAGE_RS, preserves scale"""
+    size = [tr_cfg['IMAGE_RS'], tr_cfg['IMAGE_RS']]
+    stacked_image = tf.concat([image, label], axis=-1)
+    cropped_image = tf.image.random_crop(
+        stacked_image, size=[*size, IMAGE_CH+1])
+    
+    # for label, if you want [w,h,1] shape, use -1:
+    return cropped_image[:,:,:IMAGE_CH], cropped_image[:,:,-1:]
+
+def random_crop_resize(image, label):
+    """does 2 ops
+    1. random crop with random scale as percentage from minimum length
+    2. resize the patch to size IMAGE_RS"""
+    scale = tf.random.uniform([], 0.5, 1.0, dtype=tf.float32)
+    # identify smallest edge
+    image_shape = image.get_shape()  # so I can get dims
+    if image_shape.ndims == 3:
+        less_edge = tf.math.reduce_min(tf.shape(image)[:2])
+    if image_shape.dims == 4:
+        less_edge = tf.math.reduce_min(tf.shape(image)[1:3])
+
+    # random crop
+    target_crop = tf.cast((tf.cast(less_edge, tf.float32) * scale), tf.uint32)
+    size = [target_crop, target_crop]
+    stacked_image = tf.concat([image, label], axis=-1)
+    cropped_image = tf.image.random_crop(
+        stacked_image, size=[*size, IMAGE_CH+1])
+
+    # return resize the crop
+    return resize(cropped_image[:,:,:IMAGE_CH], cropped_image[:,:,-1:])
+
+def get_reduce_res_func(
+    is_resize=1,
+    is_pad_resize=0,
+    is_crop=0,
+    is_crop_resize=0
+):
+    """acts as a function selector and also updates the cfg
+    returns a tf function that reduces resolution. does OneOf ops"""
+    if (is_resize+is_pad_resize+is_crop+is_crop_resize)>1:
+        raise ValueError('can only do 1 reduce resize ops')
+
+    if is_resize:
+        tr_cfg['REDUCE_RES'] = 'resize'
+        return resize
+    elif is_pad_resize:
+        tr_cfg['REDUCE_RES'] = 'pad_resize'
+        return pad_resize
+    elif is_crop:
+        tr_cfg['REDUCE_RES'] = 'crop'
+        return random_crop
+    elif is_crop_resize:
+        tr_cfg['REDUCE_RES'] = 'crop_resize'
+        return random_crop_resize
+    else:
+        raise ValueError('must provide reduce_res function')
+
+
+
+
+### AUGMENTATION FUNCTION ###
+def aug_tf(image, label):
+    """maps augmentation from tf based on tr_cfg"""
+    # Flips
+    if tr_cfg['IS_HFLIP']:
+        p_hflip = tf.random.uniform([], 0, 1.0, dtype=tf.float32)
+        if p_hflip >= .5:
+            image = tf.image.flip_left_right(image)
+            label = tf.image.flip_left_right(label)
+    
+    if tr_cfg['IS_VFLIP']:
+        p_vflip = tf.random.uniform([], 0, 1.0, dtype=tf.float32)
+        if p_vflip >= .5:
+            image = tf.image.flip_up_down(image)
+            label = tf.image.flip_up_down(label)
+    
+        
+    # Rotates
+    if tr_cfg['IS_ROT90']:
+        p_rotate = tf.random.uniform([], 0, 1.0, dtype=tf.float32)
+        if p_rotate > .75:
+            image = tf.image.rot90(image, k=3) # rotate 270º
+            label = tf.image.rot90(label, k=3) # rotate 270º
+        elif p_rotate > .5:
+            image = tf.image.rot90(image, k=2) # rotate 180º
+            label = tf.image.rot90(label, k=2) # rotate 180º
+        elif p_rotate > .25:
+            image = tf.image.rot90(image, k=1) # rotate 90º
+            label = tf.image.rot90(label, k=1) # rotate 90º
+
+    if tr_cfg['IS_FINE_ROT']:
+        min_rad = tr_cfg['ROT_RANGE'][0] * math.pi / 180
+        max_rad = tr_cfg['ROT_RANGE'][1] * math.pi / 180
+        rot = tf.random.uniform([], min_rad, max_rad, dtype=tf.float32)
+        image = tfa.image.rotate(image, rot)
+        label = tfa.image.rotate(label, rot)
+
+
+    # Shear
+    if tr_cfg['IS_SHEAR_X']:
+        """not optimal, shear from tfa works only for rgb
+        """
+        p_shear = tf.random.uniform([], 0, 1.0, dtype=tf.float32)
+        if p_shear > 0.5:
+            min_shear = tr_cfg['SHEAR_RANGE'][0] * math.pi / 180
+            max_shear = tr_cfg['SHEAR_RANGE'][1] * math.pi / 180
+            shear = tf.random.uniform([], min_shear, max_shear, dtype=tf.float32)
+            image = tf.image.grayscale_to_rgb(image)
+            image = tfa.image.shear_x(image, shear, 0.0)
+            image = tf.image.rgb_to_grayscale(image)
+
+            label = tf.image.grayscale_to_rgb(label)
+            label = tfa.image.shear_x(label, shear, 0.0)
+            label = tf.image.rgb_to_grayscale(label)
+           
+    if tr_cfg['IS_SHEAR_Y']:
+        p_shear = tf.random.uniform([], 0, 1.0, dtype=tf.float32)
+        if p_shear > 0.5:
+            min_shear = tr_cfg['SHEAR_RANGE'][0] * math.pi / 180
+            max_shear = tr_cfg['SHEAR_RANGE'][1] * math.pi / 180
+            shear = tf.random.uniform([], min_shear, max_shear, dtype=tf.float32)
+            image = tf.image.grayscale_to_rgb(image)
+            image = tfa.image.shear_y(image, shear, 0.0)
+            image = tf.image.rgb_to_grayscale(image)
+
+            label = tf.image.grayscale_to_rgb(label)
+            label = tfa.image.shear_y(label, shear, 0.0)
+            label = tf.image.rgb_to_grayscale(label)
+
+    return image, label
+
+
+def update_aug_tf(
+    is_hflip=0,
+    is_vflip=0,
+    is_rot90=0,
+    is_fine_rot=0,
+    is_shear_x=0,
+    is_shear_y=0,
+    rot_range=[-10,10],
+    shear_range=[-10,10]
+):
+    """configure augmentations from tf"""
+    tr_cfg['IS_HFLIP'] = is_hflip
+    tr_cfg['IS_VFLIP'] = is_vflip
+    tr_cfg['IS_ROT90'] = is_rot90
+    tr_cfg['IS_FINE_ROT'] = is_fine_rot
+    tr_cfg['IS_SHEAR_X'] = is_shear_x
+    tr_cfg['IS_SHEAR_Y'] = is_shear_y
+    tr_cfg['ROT_RANGE'] = rot_range
+    tr_cfg['SHEAR_RANGE'] = shear_range
+    
+    # toggle the map function in dataloader
+    if (is_hflip+is_vflip+is_rot90+is_fine_rot+is_shear_x+is_shear_y)>0:
+        IS_AUG_TF = 1
+    else:
+        IS_AUG_TF = 0
+        
+def update_aug_off(
+    is_elee=0,
+    is_frost=0,
+    is_gmap=0
+):
+    IS_OFF_AUG = 1
+    if is_elee:
+        tr_cfg['OFF_DS'] = 'elee'
+        SUB_PATH_TRAIN = 'elee'
+        SUB_PATH_VAL = 'elee'
+    elif is_frost:
+        tr_cfg['OFF_DS'] = 'frost'
+        SUB_PATH_TRAIN = 'frost'
+        SUB_PATH_VAL = 'frost'
+    elif is_gmap:
+        tr_cfg['OFF_DS'] = 'gmap'
+        SUB_PATH_TRAIN = 'gmap'
+        SUB_PATH_VAL = 'gmap'
+    else:
+        IS_OFF_AUG = 0
+        SUB_PATH_TRAIN = ''
+        SUB_PATH_VAL = ''
+
+
+
+
+
+
