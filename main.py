ed1c717a6090b65bb91d66645b4a834a1251a49a
sandhi_artha <sandiartha99@yahoo.co.id>
2022-04-09 02:42:24 +0200

    add main

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-# from sn6.cfg import tr_cfg
-# from sn6.model import BFE
+from sn6.cfg import tr_cfg
+from sn6.model import BFE
 
 
-# def run():
-#     """Builds model, loads data, trains and evaluates"""
-#     print('initializing..')
-#     model = BFE(tr_cfg)
-#     print('loading data..')
-#     model.load_data()
-#     # model.check_data()
-#     print('creating model..')
-#     model.build()
-#     print('starting training..')
-#     model.train()
-#     print('eval results:')
-#     model.evaluate()
+def run():
+    """Builds model, loads data, trains and evaluates"""
+    print('initializing..')
+    model = BFE(tr_cfg)
+    print('loading data..')
+    model.load_data()
+    model.dataloader.preview_train_ds(n_show=4, n_rep=1, min_view=True)
+    print('creating model..')
+    model.load_model()
+    print('starting training..')
+    model.train(verbose=1)
+    print('eval results:')
+    model.view_results(n_show=6)
 
 if __name__ == '__main__':
-    # run()
-    a = 'a'
-    b = 's'
-    if a:
-        print('a')
-    elif b:
-        print('b')
-    else:
-        print('c')
+    run()
